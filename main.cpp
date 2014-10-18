c4ab95bcae2bdda89b0173720cbd9b8b0ede5346
Gillian Z Liu <gillianzliu@gmail.com>
2014-10-17 17:05:38 -0700

    added function get command, still in progress. Only makes tokens right now, also can see what delimiter it stopped at

--- main.cpp
+++ main.cpp
@@ -1,17 +1,46 @@
 #include <iostream>
-
+#include <string>
+#include <cstring>
 
 using namespace std;
 
+char ** get_command(string a)
+{
+    char * copy = new char [a.length() + 1];
+    strcpy(copy, a.c_str());
+
+    //bool done = false;
+    char * begin = copy;
+    char * token = strtok(copy, " ;|&");
+
+    while (token != 0)
+    {
+        cout << token << endl;
+
+        if (token - begin + strlen(token) < a.size())
+        {
+            cout << a.at(token - begin + strlen(token)) << endl;
+        }
+
+        token = strtok(NULL, " ;|&");
+    }
+
+    return 0;
+}
+
 void display_prompt()
 {
-    cout << "$";
+    cout << "$ ";
 }
 
 int main()
 {
     display_prompt();
-    string command = getline();
+    string command;
+    getline(cin, command);
     cout << command << endl;
-    return 0,
+
+    get_command(command);
+
+    return 0;
 }
